61df20f374016d021fa9673238bd57510ec00681
Magnus Ihse Bursie <mag@icus.se>
2017-07-17 16:58:29 +0200

    Cleanup ncid fake server

--- tests/components/binary_sensor/test_ncid.py
+++ tests/components/binary_sensor/test_ncid.py
@@ -23,37 +23,43 @@ TEST_CONFIG = {
     },
 }
 
-class MyNCIDHandler(socketserver.StreamRequestHandler):
-    def handle(self):
-        print("in handle")
-        # output = 'CIDINFO: *LINE*4901*RING*-2*TIME*00:11:01*'
-        output = 'OUT: *DATE*01152017*TIME*0010*LINE*4901*NMBR*012345611*MESG*NONE*NAME*NO NAME*'
+class FakeNCIDServer:
+    class ReuseAddressThreadingTCPServer(socketserver.ThreadingTCPServer):
+        allow_reuse_address = True
 
-        self.wfile.write(bytearray(output, 'utf8'))
-        self.wfile.flush()
+    class MyNCIDHandler(socketserver.StreamRequestHandler):
+        def handle(self):
+            print("in handle")
+            # output = 'CIDINFO: *LINE*4901*RING*-2*TIME*00:11:01*'
+            output = 'OUT: *DATE*01152017*TIME*0010*LINE*4901*NMBR*012345611*MESG*NONE*NAME*NO NAME*'
 
-class ReuseAddressThreadingTCPServer(socketserver.ThreadingTCPServer):
-    allow_reuse_address = True
+            self.wfile.write(bytearray(output, 'utf8'))
+            self.wfile.flush()
 
-class TestNCIDBinarySensor(unittest.TestCase):
-    """Test the ncid Caller ID service."""
+    def __init__(self, address):
+        self._server = FakeNCIDServer.ReuseAddressThreadingTCPServer(address, FakeNCIDServer.MyNCIDHandler)
 
-    def _start_fake_server(self):
-        self._server = ReuseAddressThreadingTCPServer((TEST_HOST, TEST_PORT), MyNCIDHandler)
-
+    def start(self):
         self._server_thread = threading.Thread(target=self._server.serve_forever)
         self._server_thread.daemon = True
         self._server_thread.start()
 
+    def stop(self):
+        self._server.shutdown()
+        self._server_thread.join()
+
+class TestNCIDBinarySensor(unittest.TestCase):
+    """Test the ncid Caller ID service."""
+
     def setUp(self):  # pylint: disable=invalid-name
         """Setup things to be run when tests are started."""
         self.hass = get_test_home_assistant()
-        self._start_fake_server()
+        self.server = FakeNCIDServer((TEST_HOST, TEST_PORT))
+        self.server.start()
 
     def tearDown(self):  # pylint: disable=invalid-name
         """Stop everything that was started."""
-        self._server.shutdown()
-        self._server_thread.join()
+        self.server.stop()
         self.hass.stop()
 
     def test_setup_platform_valid_config(self):
